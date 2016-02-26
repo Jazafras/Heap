039615bc6a6544e20b2b7fe265c7a49c7f5847dc
Jasmine Emerson <Jazafras@users.noreply.github.com>
2016-02-25 17:59:09 -0800

    Added files via upload

---
+++ Heap.java
@@ -0,0 +1,124 @@
+//Jasmine Emerson
+//CS 122
+//Assignment 8
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Heap {
+
+	int [] heap;
+	int size;
+	public Heap() {
+		heap = new int[100000];
+	}
+	
+	public void add(int e) {
+		size++;
+		int i = size - 1; //start at last element
+		while (i > 0 && heap[(i-1)/2] > e) //new element is less than its parent
+		{
+			heap[i] = heap[(i-1)/2]; //make child the parent
+			i = (i-1)/2; //check next parent
+		}
+		heap[i] = e;
+	}
+	public long removeMin() {
+		int min = 0;
+		if (size < 1) //nothing is in the heap
+		{
+			System.out.println("error");
+		}
+		else
+		{
+			min = heap[0]; //assign the root as the minimum
+			heap[0] = heap[size-1]; //last element is the new root 
+			size--; //decrement the size
+			minHeapify(0); //heapify the new root
+		}
+		return min; //return what was removed
+	}
+	
+	private void minHeapify(int index) {
+		int left, right, min, fluff;
+		left = (index*2) + 1; //left child
+		right = (index*2) + 2; //right child
+		if (left < size && heap[left] < heap[index]) //left child is less than the parent
+		{
+			min = left; //left child is the new minimum
+		}
+		else
+		{
+			min = index; //parent is still the minimum
+		}
+		if (right < size && heap[right] < heap[min]) //right child is less that the current minimum
+		{
+			min = right; //right child is the new minimum
+		}
+		if (min != index)
+		{
+			fluff = heap[index];
+			heap[index] = heap[min]; //put the parent in the child's spot
+			heap[min] = fluff; //put the child in the parent's spot
+			minHeapify(min); //do the process over
+		}
+	}
+	
+	public static void main(String[] args) {
+		Heap test = new Heap ();
+		test.add(10);
+		test.add(30);
+		test.add(4);
+		test.add(5);
+		test.add(64);
+		test.add(7);
+		test.add(2);
+		test.add(8);
+		for (int i = 0; i < test.size; i++)
+		{
+			System.out.println(test.heap[i]);
+		}
+		test.removeMin();
+		System.out.println("Removed minimum");
+		for (int i = 0; i < test.size; i++)
+		{
+			System.out.println(test.heap[i]);
+		}
+//
+//		long startTime = System.currentTimeMillis();
+//		File file = new File("values.txt");
+//		Scanner values;
+//		
+//		//adding values to heap
+//		try 
+//		{
+//			values = new Scanner(file); //scan text file
+//			
+//			while (values.hasNextInt()) //checking if the next line is an integer
+//			{
+//				int i = values.nextInt(); 
+//				test.add(i);
+//			}
+//			values.close();
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		for (int i = 0; i < test.size; i++)
+//		{
+//			System.out.println(test.heap[i]);
+//		}
+//		long endTime = System.currentTimeMillis();
+//		System.out.println("It took " + (endTime - startTime) + " milliseconds to insert all the values into the heap");
+//		
+//		//removing from the heap
+//		long startTimeRemove = System.currentTimeMillis();
+//		for (int i = 0; i < test.size; i++) //go through the whole heap
+//		{
+//			test.removeMin();
+//		}
+//		long endTimeRemove = System.currentTimeMillis();
+//		System.out.println("It took " + (endTimeRemove - startTimeRemove) + " milliseconds to remove all the values from the heap");
+	}
+}
